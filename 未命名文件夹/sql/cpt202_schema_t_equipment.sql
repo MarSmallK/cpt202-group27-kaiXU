0a9a0c3e2b5dd414131422c32634488f315e824f
MarSmallK <2667952546@qq.com>
2024-05-01 12:22:43 +0800

    xk-sql

---
+++ 未命名文件夹/sql/cpt202_schema_t_equipment.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+--
+-- Host: localhost    Database: cpt202_schema
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_equipment`
+--
+
+DROP TABLE IF EXISTS `t_equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `t_equipment` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `equipment_type` varchar(4) DEFAULT NULL COMMENT '器材类型',
+  `equipment_code` varchar(40) DEFAULT NULL COMMENT '器材编码',
+  `equipment_price` varchar(40) DEFAULT NULL COMMENT '设备价格',
+  `equipment_photo` varchar(100) DEFAULT NULL COMMENT '照片',
+  `add_time` datetime DEFAULT NULL COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='设备表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_equipment`
+--
+
+LOCK TABLES `t_equipment` WRITE;
+/*!40000 ALTER TABLE `t_equipment` DISABLE KEYS */;
+INSERT INTO `t_equipment` VALUES (2,'1','002','30',NULL,'2024-04-25 16:37:40'),(4,'1','004','30',NULL,'2024-04-25 16:39:21'),(5,'2','005','400',NULL,'2024-04-25 16:39:22'),(6,'2','006','400',NULL,'2024-04-25 16:39:23'),(7,'2','007','400',NULL,'2024-04-25 16:39:25'),(8,'2','008','400',NULL,'2024-04-25 16:39:26'),(9,'2','009','400',NULL,'2024-04-25 16:39:27'),(11,'2','011','400',NULL,'2024-04-25 16:39:30'),(12,'2','012','400',NULL,'2024-04-25 16:39:31'),(13,'2','013','400',NULL,'2024-04-25 16:39:32'),(14,'1',NULL,'1',NULL,NULL),(15,'1',NULL,'',NULL,NULL),(16,'3','1314675','12367896',NULL,'2024-04-25 22:09:47');
+/*!40000 ALTER TABLE `t_equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-01 12:21:20
---
+++ 未命名文件夹/sql/cpt202_schema_trainer.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+--
+-- Host: localhost    Database: cpt202_schema
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `trainer`
+--
+
+DROP TABLE IF EXISTS `trainer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trainer` (
+  `trainer_ID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `sex` varchar(45) NOT NULL,
+  `honour` varchar(45) DEFAULT NULL,
+  `experience` varchar(45) DEFAULT NULL,
+  `entrytime` datetime NOT NULL,
+  `starlevel` varchar(45) NOT NULL DEFAULT '3rd',
+  PRIMARY KEY (`trainer_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trainer`
+--
+
+LOCK TABLES `trainer` WRITE;
+/*!40000 ALTER TABLE `trainer` DISABLE KEYS */;
+INSERT INTO `trainer` VALUES (1,'Mark','Male','1st','5y','2024-03-24 00:00:00','3rd'),(2,'Lily','Female','2st','8y','2022-07-14 00:00:00','5rd'),(5,'m','m','m','m','2024-03-21 00:00:00','m'),(6,'m','m11111','m','m','2024-03-21 14:00:00','m'),(8,'x','x','','','2024-04-24 00:00:00','5');
+/*!40000 ALTER TABLE `trainer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-01 12:21:20
