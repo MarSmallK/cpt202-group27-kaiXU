0ec7b6981743b936e16cda57b823fcee5ab42355
MarSmallK <2667952546@qq.com>
2024-05-03 19:28:42 +0800

    xk

---
+++ 未命名文件夹/cpt202B/src/main/java/group27/xukai/cpt202b/service/SendNotInterface.java
@@ -0,0 +1,9 @@
+package group27.xukai.cpt202b.service;
+
+import group27.xukai.cpt202b.entity.Notification;
+
+public interface SendNotInterface {
+    void sendNot(Notification notification);
+
+    void SendNot(Notification notification);
+}
